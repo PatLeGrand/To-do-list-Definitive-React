47b102f93a2928f455c44f0282fca9dee714c2bb
PatLeGrand <aurelpatrickm@gmail.com>
2026-01-27 22:03:35 -0500

    fix:style for header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,13 @@
 import reactLogo from '../../assets/react.svg'
 export const Header = () => {
     return (
-        <div className="flex justify-between  items-center p-3 mb-5">
+        <div className="flex justify-between  items-center p-3 mb-5 pl-0">
             <div>
                 <div className="flex gap-3 ">
                     <img src={reactLogo} alt="React Logo" width={50}   height={50} />
                     <div>
-                        <h1 className="text-4xl ">TaskManager</h1>
-                        <code className="mt-2">Éliminez le chaos, suivez le flux.</code>
+                        <h1 className="text-4xl text-primary">TaskManager</h1>
+                        <code className="mt-2 text-secondary">Éliminez le chaos, suivez le flux.</code>
                     </div>
                 </div>
             </div>
